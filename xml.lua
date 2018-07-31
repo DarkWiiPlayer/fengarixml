0c2ba4292df1680fc2e5a5ba292b6a323ef1fd00
DarkWiiPlauer <darkwiiplayer@hotmail.com>
2018-07-31 11:19:22 +0200

    Change everything to fengari (untested)
    For now everything is changed to generate DOM nodes using fengari, but
    there hasn't been any testing yet, so things may (will) probably not
    work out of the box.

--- xml.lua
+++ xml.lua
@@ -1,30 +1,3 @@
-local void
-do
-  local _tbl_0 = { }
-  local _list_0 = {
-    "area",
-    "base",
-    "br",
-    "col",
-    "command",
-    "embed",
-    "hr",
-    "img",
-    "input",
-    "keygen",
-    "link",
-    "meta",
-    "param",
-    "source",
-    "track",
-    "wbr"
-  }
-  for _index_0 = 1, #_list_0 do
-    local key = _list_0[_index_0]
-    _tbl_0[key] = true
-  end
-  void = _tbl_0
-end
 local escapes = {
   ['&'] = '&amp;',
   ['<'] = '&lt;',
@@ -33,7 +6,24 @@ local escapes = {
   ["'"] = '&#039;'
 }
 local env
-env = function(make_tag)
+do
+  local createelement, createtext
+  do
+    local global = require('js').global
+    createelement, createtext = (function()
+      local _base_0 = global
+      local _fn_0 = _base_0.createElement
+      return function(...)
+        return _fn_0(_base_0, ...)
+      end
+    end)(), (function()
+      local _base_0 = global
+      local _fn_0 = _base_0.createTextNode
+      return function(...)
+        return _fn_0(_base_0, ...)
+      end
+    end)()
+  end
   local environment = setmetatable({ }, {
     __index = function(self, key)
       return (_ENV or _G)[key] or function(...)
@@ -47,15 +37,6 @@ env = function(make_tag)
     return (function(self)
       return self
     end)(tostring(value):gsub([[[<>&]'"]], escapes))
-  end
-  local split
-  split = function(tab)
-    local ary = { }
-    for k, v in ipairs(tab) do
-      ary[k] = v
-      tab[k] = nil
-    end
-    return ary, tab
   end
   local flatten
   flatten = function(tab, flat)
@@ -79,93 +60,88 @@ env = function(make_tag)
     end
     return flat
   end
-  tag = function(tagname, ...)
-    return make_tag(print, tagname, split(flatten({
+  local split
+  split = function(tab)
+    local ary = { }
+    for k, v in ipairs(tab) do
+      ary[k] = v
+      tab[k] = nil
+    end
+    return ary, tab
+  end
+  local attrib
+  attrib = function(args)
+    local res = setmetatable({ }, {
+      __tostring = function(self)
+        local tab
+        do
+          local _accum_0 = { }
+          local _len_0 = 1
+          for key, value in pairs(self) do
+            if type(value) == 'string' or type(value) == 'number' then
+              _accum_0[_len_0] = tostring(key) .. "=\"" .. tostring(value) .. "\""
+              _len_0 = _len_0 + 1
+            end
+          end
+          tab = _accum_0
+        end
+        return #tab > 0 and ' ' .. table.concat(tab, ' ') or ''
+      end
+    })
+    for key, value in pairs(args) do
+      if type(key) == 'string' then
+        res[key] = value
+        local r = true
+      end
+    end
+    return res
+  end
+  local handle
+  handle = function(parent, args)
+    for _index_0 = 1, #args do
+      local arg = args[_index_0]
+      local _exp_0 = type(arg)
+      if 'table' == _exp_0 then
+        handle(arg)
+      elseif 'function' == _exp_0 then
+        arg()
+      else
+        print(tostring(arg))
+      end
+    end
+    return parent
+  end
+  node = function(nodename, ...)
+    local attributes, content = (function(a, c)
+      return attrib(a), b
+    end)(split(flatten({
       ...
     })))
+    local new = handle(create_node(nodename), attrib)
   end
   return environment
 end
-local make
-if _VERSION == 'Lua 5.1' then
-  make = function(environment)
-    return function(fnc)
-      assert(type(fnc) == 'function', 'wrong argument to render, expecting function')
-      setfenv(fnc, environment)
-      return function(out, ...)
-        if out == nil then
-          out = print
-        end
-        environment.print = out
-        return fnc(...)
-      end
-    end
+local _
+_ = function(fnc)
+  assert(type(fnc) == 'function', 'wrong argument to render, expecting function')
+  do
+    local upvaluejoin = debug.upvaluejoin
+    local _ENV = environment
+    upvaluejoin(fnc, 1, (function()
+      return aaaa()
+    end), 1)
   end
-else
-  make = function(environment)
-    return function(fnc)
-      assert(type(fnc) == 'function', 'wrong argument to render, expecting function')
-      do
-        local upvaluejoin = debug.upvaluejoin
-        local _ENV = environment
-        upvaluejoin(fnc, 1, (function()
-          return aaaa()
-        end), 1)
-      end
-      return function(out, ...)
-        if out == nil then
-          out = print
-        end
-        environment.print = out
-        return fnc(...)
-      end
+  return function(out, ...)
+    if out == nil then
+      out = print
     end
+    environment.print = out
+    return fnc(...)
   end
 end
 local render
 render = function(out, fnc)
   return build(fnc)(out)
-end
-local attrib
-attrib = function(args)
-  local res = setmetatable({ }, {
-    __tostring = function(self)
-      local tab
-      do
-        local _accum_0 = { }
-        local _len_0 = 1
-        for key, value in pairs(self) do
-          if type(value) == 'string' or type(value) == 'number' then
-            _accum_0[_len_0] = tostring(key) .. "=\"" .. tostring(value) .. "\""
-            _len_0 = _len_0 + 1
-          end
-        end
-        tab = _accum_0
-      end
-      return #tab > 0 and ' ' .. table.concat(tab, ' ') or ''
-    end
-  })
-  for key, value in pairs(args) do
-    if type(key) == 'string' then
-      res[key] = value
-      local r = true
-    end
-  end
-  return res
-end
-local handle
-handle = function(print, args)
-  for _index_0 = 1, #args do
-    local arg = args[_index_0]
-    local _exp_0 = type(arg)
-    if 'table' == _exp_0 then
-      handle(arg)
-    elseif 'function' == _exp_0 then
-      arg()
-    else
-      print(tostring(arg))
-    end
-  end
 end
 local environment = {
   xml = env(function(print, tagname, inner, args)
@@ -176,24 +152,9 @@ local environment = {
     if not ((#inner == 0)) then
       return print("</" .. tostring(tagname) .. ">")
     end
-  end),
-  html = env(function(print, tagname, inner, args)
-    if not (void[tagname] and #inner == 0) then
-      print("<" .. tostring(tagname) .. tostring(attrib(args)) .. ">")
-      if not (#inner == 0) then
-        handle(print, inner)
-      end
-      return print("</" .. tostring(tagname) .. ">")
-    else
-      return print("<" .. tostring(tagname) .. tostring(attrib(args)) .. ">")
-    end
   end)
 }
-environment.html.html5 = function()
-  return environment.html.print('<!doctype html5>')
-end
 return {
   environment = environment,
-  xml = make(environment.xml),
-  html = make(environment.html)
+  xml = make(environment.xml)
 }
--- xml.moon
+++ xml.moon
@@ -1,37 +1,18 @@
-void = {key,true for key in *{
-	"area", "base", "br", "col"
-	"command", "embed", "hr", "img"
-	"input", "keygen", "link", "meta"
-	"param", "source", "track", "wbr"
-}}
+createelement, createtext = do
+	global = require'js'.global
+	global\createElement, global\createTextNode
 
-escapes = {
-	['&']: '&amp;'
-	['<']: '&lt;'
-	['>']: '&gt;'
-	['"']: '&quot;'
-	["'"]: '&#039;'
-}
-
-env = (make_tag) ->
-	environment = setmetatable {}, {
+environment = do
+	env = setmetatable {}, {
 		__index: (key) =>
 			(_ENV or _G)[key] or (...) ->
 				@.tag(key, ...)
 	}
+	_ENV, env = env, nil
 
-	_G   = environment -- Local, doesn't override global _G in 5.2+
-	_ENV = environment -- Local, doesn't affect outside world anyway
-
+	--TODO: Remove
 	export escape = (value) ->
 		(=>@) tostring(value)\gsub [[[<>&]'"]], escapes
-
-	split = (tab) ->
-		ary = {}
-		for k,v in ipairs(tab) do
-			ary[k]=v
-			tab[k]=nil
-		return ary, tab
 
 	flatten = (tab, flat={}) ->
 		for key, value in pairs tab
@@ -47,73 +28,48 @@ env = (make_tag) ->
 					flat[key] = value
 		flat
 
-	export tag = (tagname, ...) ->
-		make_tag(print, tagname, split flatten {...})
+	split = (tab) ->
+		ary = {}
+		for k,v in ipairs(tab) do
+			ary[k]=v
+			tab[k]=nil
+		return ary, tab
 
-	return environment
+	--- Handles inner HTML for a node
+	handle_inner = (element, inner) ->
+		for arg in *args
+			switch type(arg)
+				when 'table'
+					handle arg
+				when 'function'
+					previous = Parent
+					export Parent = element
+					arg! -- TODO: do something about context (parent element)
+					export Parent = previous
+				else
+					parent\appendChild createtextnode tostring arg
+		element
 
-make = if _VERSION == 'Lua 5.1' then
-	(environment) ->
-		(fnc) ->
-			assert(type(fnc)=='function', 'wrong argument to render, expecting function')
-			setfenv(fnc, environment)
-			return (out=print, ...) ->
-				environment.print = out
-				return fnc(...)
-else
-	(environment) ->
-		(fnc) ->
-			assert(type(fnc)=='function', 'wrong argument to render, expecting function')
-			do
-				upvaluejoin = debug.upvaluejoin
-				_ENV = environment
-				upvaluejoin(fnc, 1, (-> aaaa!), 1) -- Set environment
-			return (out=print, ...) ->
-				environment.print = out
-				return fnc(...)
+	export node = (nodename, ...) ->
+		attributes, content = split flatten {...}
+		New = handle create_node(nodename), attrib
+		for key, value in pairs(attributes)
+			New\setAttribute tostring(key), tostring(value)
+		Parent\appendChild New
 
-render = (out, fnc) ->
-	build(fnc)(out)
+	return _ENV
 
-attrib = (args) ->
-	res = setmetatable {}, __tostring: =>
-		tab = ["#{key}=\"#{value}\"" for key, value in pairs(@) when type(value)=='string' or type(value)=='number']
-		#tab > 0 and ' '..table.concat(tab,' ') or ''
-	for key, value in pairs(args)
-		if type(key)=='string'
-			res[key] = value
-			r = true
-	return res
-
---- Handles the inner HTML of a tag
-handle = (print, args) ->
-	for arg in *args
-		switch type(arg)
-			when 'table'
-				handle arg
-			when 'function'
-				arg!
-			else
-				print tostring arg
-
-environment = {
-	xml: env (print, tagname, inner, args) ->
-		print "<#{tagname}#{attrib args}#{#inner==0 and ' /' or ''}>"
-		handle print, inner unless #inner==0
-		print "</#{tagname}>" unless (#inner==0)
-	html: env (print, tagname, inner, args) ->
-		unless void[tagname] and #inner==0
-			print "<#{tagname}#{attrib args}>"
-			handle print, inner unless #inner==0
-			print "</#{tagname}>"
-		else
-			print "<#{tagname}#{attrib args}>"
-}
-environment.html.html5 = ->
-	environment.html.print '<!doctype html5>'
+build = (fnc) ->
+	assert(type(fnc)=='function', 'wrong argument type, expecting function')
+	do
+		upvaluejoin = debug.upvaluejoin
+		_ENV = environment
+		upvaluejoin(fnc, 1, (-> aaaa!), 1) -- Set environment
+	return (parent, ...) ->
+		environment.Parent = parent
+		return fnc(...)
 
 {
 	:environment
-	xml: make environment.xml
-	html: make environment.html
+	:make
 }
